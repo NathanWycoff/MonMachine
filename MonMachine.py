557ec0263a550e8f1d965dc7e861dee53ee5787a
Nathan Wycoff <nathanbrwycoff@gmail.com>
2016-11-11 22:08:01 -0500

    AI Stage 1

--- MonMachine.py
+++ MonMachine.py
@@ -14,6 +14,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 import numpy as np
+import csv
 
 
 #Represents a pokemon
@@ -54,7 +55,10 @@ class MonWrangler(object):
     def get_random_alive_mon(self):
         alive_mons = [x for x in self.mons if x.is_alive]
         d = np.random.randint(0,len(alive_mons))
-        return(alive_mons[d])
+        if d > 0:
+            return(alive_mons[d])
+        else:
+            return(None)
     
     #Track a dead mon
     def kill(self, ID):
@@ -68,8 +72,12 @@ class MonWrangler(object):
 #Takes data from the webbrowser and feeds it to the agent in a 
 #reinfocerment learning theoretic manner.
 class Environment(object):
-    def __init__(self, log):
+    def __init__(self, log, agent):
         print "Environment is initing..."
+        
+        #Our AI
+        self.agent = agent
+        
         #Roster of mons
         self.kevin = MonWrangler([Mon(x) for x in [1,2,3,4,5,6]])
         
@@ -137,11 +145,18 @@ class Environment(object):
                 
                 #Check if the battle's over
                 if re.match('.* won the battle!', entry) is not None:
-                    if re.match('monmachine',entry) is not None:
+                    
+                    if re.match('MonMachine',entry) is not None:
                         print "We win!"
+                        self.agent.reward(100.0, 'Terminal')
                     else:
                         print "GG, we lost"
+                        self.agent.reward(0.0, 'Terminal')
+                    
                     self.in_game = False
+                    
+                    agent.save()
+                    print 'Q and V updated'
                 
             
             #Otherwise, if at least one turn passed, make a move
@@ -152,13 +167,15 @@ class Environment(object):
                         
     #Main action function, for now move randomly
     def take_turn(self):
-        d = np.random.randint(1,4)
+        d = self.agent.act(self.current_mon.ID)
+        self.agent.reward(0.0, self.current_mon.ID)
         try:
             driver.find_element_by_xpath("(//button[@name='chooseMove'])[" + str(d) + "]").click()
-        except:#Caterpies only have moves in 1,2
+        except:#Maybe we did hyperbeam?
             d = 1
             driver.find_element_by_xpath("(//button[@name='chooseMove'])[" + str(d) + "]").click()
         
+    
     def switch_to_mon(self, mon):
         self.current_mon = mon
         to = mon.ID
@@ -170,13 +187,92 @@ class Environment(object):
     #For now, just summon a random mon
     def funeral(self):
         self.kevin.kill(self.current_mon.ID)
-        self.switch_to_mon(self.kevin.get_random_alive_mon())
+        if False:#Next Mon is random
+            next_mon = self.kevin.get_random_alive_mon()
+            if next_mon is not None:
+                self.switch_to_mon(next_mon)
+        else:
+            try:
+                next_mon = self.kevin.get_mon(self.current_mon.ID + 1)
+                self.switch_to_mon(next_mon)
+            except:
+                print "Out of Mons!"
     
     #This function updates the turn counter and prints it out.
     def update_turn(self, turn):        
         self.current_turn = int(turn)
         print "It is now turn " + str(turn)
 
+#Q learning with epsilon greedy action selection
+class Agent(object):
+    def __init__(self, load = False):
+        #Parameters
+        self.epsilon = 0.5#Probability of exploratory action
+        self.alpha = 0.5#Learning rate
+        self.gamma = 1#Discount Rate
+        
+        #Initialize spaces
+        self.state_space = [1,2,3,4,5,6]#State space is just what mon I have
+        self.action_space = [1,2,3,4]
+        
+        #Initialize state and action value functions, or load them from dics
+        if not load:
+            self.V = [np.random.normal() for x in self.state_space]
+            self.Q = [[np.random.normal() for x in self.action_space] for y in self.state_space]
+        else:#This code copied from http://stackoverflow.com/questions/19838380/building-list-of-lists-from-csv-file
+            #Read in old V
+            with open('/home/nathan/Documents/Self Study/MonMachine/learned_V.csv', 'rU') as f:  #opens PW file
+                self.V = list([float(x) for x in list(rec)] for rec in csv.reader(f, delimiter=','))
+            
+            #Read in old Q
+            with open('/home/nathan/Documents/Self Study/MonMachine/learned_Q.csv', 'rU') as f:  #opens PW file
+                self.Q = list([float(x) for x in list(rec)] for rec in csv.reader(f, delimiter=','))
+        
+        #Other Initialization
+        self.last_state = 1
+        self.last_action = 1
+    
+    #Get an action from an epsilon greedy policy.
+    def act(self, state):
+        #If we do a random action
+        if (np.random.uniform() < self.epsilon):
+            print 'Taking an exploratory action.'
+            d = np.random.randint(1,4)
+            action = d
+        else:
+            print 'Taking a greedy action.'
+            action = self.Q[state-1].index(max(self.Q[state-1])) + 1
+        
+        #Set up reward to be updated
+        self.last_action = action
+        self.last_state = state
+        
+        return(action)
+    
+    #Call when a reward is given to update Q and V
+    def reward(self, reward, new_state):
+        #Update Q
+        optimal_value = max(self.Q[new_state-1]) if new_state != 'Terminal' else 0
+        current_q = self.Q[self.last_state-1][self.last_action-1]
+        self.Q[self.last_state-1][self.last_action-1] = current_q + self.alpha * \
+            (reward + self.gamma * optimal_value - current_q)
+        
+        #Update V, not necessary for Q learning, but may be interesting to look at.
+        for i in range(len(self.V)):
+            optimal_action = self.Q[self.last_state-1].index(max(self.Q[self.last_state-1])) + 1
+            self.V[i] = sum([self.epsilon / 3.0 * self.Q[i][x-1] if x != optimal_action else self.epsilon * self.Q[i][x-1] for x in self.action_space])
+    
+    def save(self):
+        target = '/home/nathan/Documents/Self Study/MonMachine/learned_Q.csv'
+        with open(target, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.Q)
+        
+        target = '/home/nathan/Documents/Self Study/MonMachine/learned_V.csv'
+        with open(target, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.Q)
+    
 ###################################
 #######   INITIALIZATION  #########
 ###################################
@@ -219,15 +315,34 @@ time.sleep(1)
 while driver.current_url == original_url:
     time.sleep(1)
 
+#
+#Repeatedly play someone
+#
 
-###################################
-#####   MAIN GAMEPLY CYCLE  #######
-###################################
-
-log = driver.find_element_by_css_selector('.battle-log')
-env = Environment(log)
-i = 0
-while env.in_game:
-    i += 1
-    env.refresh()
-    time.sleep(2)
+games = 0
+while True:
+    
+    print "Beginning Game " + str(games)
+    
+    driver.find_element_by_name("finduser").click()
+    driver.find_element_by_name("data").clear()
+    driver.find_element_by_name("data").send_keys("MonTonSoup")
+    driver.find_element_by_css_selector("button[type=\"submit\"]").click()
+    driver.find_element_by_name("challenge").click()
+    driver.find_element_by_name("format").click()
+    driver.find_element_by_xpath("(//button[@name='selectFormat'])[70]").click()
+    driver.find_element_by_name("makeChallenge").click()
+    
+    ###################################
+    #####   MAIN GAMEPLY CYCLE  #######
+    ###################################
+    
+    log = driver.find_element_by_css_selector('.battle-log')
+    agent = Agent(load=True)
+    env = Environment(log, agent)
+    i = 0
+    env.in_game = True
+    while env.in_game:
+        i += 1
+        env.refresh()
+        time.sleep(2)
