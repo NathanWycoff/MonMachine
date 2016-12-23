fedec2e66e33ba8815cc4f75663ae4bcb0c3bad5
Nathan Wycoff <nathanbrwycoff@gmail.com>
2016-12-23 18:47:43 -0500

    Added splitting functionality to ANN

--- Agents/artificial_neural_net.py
+++ Agents/artificial_neural_net.py
@@ -16,18 +16,25 @@ import theano.tensor as T
 #Numpy is as well.
 import numpy as np
 
-
 class artifical_neural_net(object):
     """
-    ANN/Multilayer Perceptron. A set of Layers and functions to train them.
+    ANN/Multilayer Perceptron. A set of Layers and functions to train them using Theano
+    
+    In particular, the class solves regression problems and uses logistic nonlinearity,
+    but it should be simple to modify it to do classification or use other activations funcs.
+    
+    This class can merge certain input features, and split them later in training.
+    See: http://papers.nips.cc/paper/531-node-splitting-a-constructive-algorithm-for-feed-forward-neural-networks.pdf
+    , except we pick when they are to be merged; it isn't inferred, and we start
+    all merged roots at their parents value (no s.d. shift).
     """
-    def __init__(self, rng, in_size, out_size, h, h_size, eta = 0.01, max_err = 10.0):
+    def __init__(self, rng, in_size, out_size, h, h_size, eta = 0.01, max_err = 10.0, to_merge = []):
         """
         :type rng: numpy.random.RandomState
         :param rng: a random number generator used to initialize weights
         
         :type in_size: int
-        :param len_in: Dimensionality of input space
+        :param len_in: Dimensionality of input space BEFORE ANY MERGING
         
         :type out_size: int
         :param len_out: Dimensionality of output space
@@ -43,13 +50,20 @@ class artifical_neural_net(object):
         
         :type max_err: double
         :param max_err: max_err is the absolute maximum error gradient; non-neg
+        
+        :type to_merge: list
+        :param to_merge: list of lists containing input dimensions which should
+        be merged to begin with. 
         """
+        
         X_var = T.dmatrix('X')#Represents input
         y_var = T.dmatrix('y')#Represents output
         
         ##Create and store the layers
         self.layers = []#Storage for layers
-        len_in = in_size#The input of the first layer is the dim of the input space
+        dim_red = len([item for sublist in to_merge for item in sublist])
+        starting_in_size = in_size + len(to_merge) - dim_red#Get in_size after merges
+        len_in = starting_in_size#The input of the first layer is the dim of the input space
         ID = 0#ID for each layer
         
         #Hidden layers
@@ -66,11 +80,13 @@ class artifical_neural_net(object):
             
             #Increment ID
             ID += 1
-            
+        
         #Output layer
-        self.layers.append(Layer(rng, in_vec, len_in, out_size, ID))
-        #Use this function to predict vals.
-        self.predict = theano.function([X_var], self.layers[-1].output)
+        self.layers.append(Layer(rng, in_vec, len_in, out_size, '-1'))
+        
+        #Use this function to predict vals, just need to preprocess the X.
+        predict = theano.function([X_var], self.layers[-1].output)
+        self.predict = lambda x: predict(self._get_merged_des_mat(x, self.current_merge))
         
         ##Prepare cost function for SGD
         cost = T.mean(T.square(self.layers[-1].output - y_var))
@@ -85,8 +101,13 @@ class artifical_neural_net(object):
                 for grad, layer in zip(grads, self.layers)
             ]
             
-        self.sgd = theano.function(inputs = [X_var,y_var], updates = updates)
-
+        self._sgd = theano.function(inputs = [X_var,y_var], updates = updates)
+        
+        #Which dims should we merge?
+        self.to_merge = to_merge
+        self.current_merge = to_merge[:]
+        self.prev_split = []#Contains the merges we've split
+        
         
     def grad_on(self, x, y):
         """
@@ -99,8 +120,109 @@ class artifical_neural_net(object):
         :type y: output matrix (response)
         """
         
-        self.sgd(x, y)
+        #If there's nothing to merge, don't even bother putting it through the function
+        if len(self.to_merge) > 0:
+            self._sgd(self._get_merged_des_mat(x, self.current_merge), y)
+        else:
+            self._sgd(x, y)
+    
+    def _get_merged_des_mat(self, X, to_merge = []):
+        """
+        Sums columns of the design matrix together as specified to create a smaller
+        design matrix. The purpose of this is to get around Theano inability to 
+        take gradients wrt a function involving T.repeats with unknown repeats.
         
+        Returns an np.array of reduced dim. if to_merge is an empty list, simply
+        returns X. The merged columns are added to the end.
+        
+        Meant for internal use.
+        
+        :type to_merge: list
+        :param to_merge: list of lists, each sublist contains columns to combine.
+        """
+        
+        merged_inds = [item for sublist in to_merge for item in sublist]#Unlist everything
+        
+        #Check that there are no duplicates
+        if len(merged_inds) != len(set(merged_inds)):
+            print "ERR in get_merged_des_mat: Duplicate Columns Merged"
+            return(0)
+        
+        #Get a list of ALL the merges, including when a column is only be merged with itself.
+        not_to_merge = list(set(range(np.shape(X)[1])).difference(merged_inds))
+        merged = [[x] for x in not_to_merge] + to_merge
+        
+        #Merge every column as specified above.
+        return(np.array([sum([X[:,j] for j in l]) for l in merged]).T)
+    
+    def _de_merge_w(self, w, to_merge, which_split, previously_split = []):
+        """
+        Calculates the new weight vector when formerly merged columns are split.
+        
+        Meant for internal use.
+        
+        TODO: Make more readable/simplify code, surely there's a better way to write this.
+        
+        :type w: np 1d array
+        :param w: weight vector to be split
+        
+        :type to_merge: list
+        :param to_merge: the merge configuration BEFORE any splits; original to_merge
+        
+        :type which_split: int
+        :param which_split: index of merger in to_merge which is being split.
+        
+        :type previously_split: list
+        :param previously_split: list of integer indexes of places previously split
+        """
+        
+        fixed_cols = []
+        for col_i in range(np.shape(w)[1]):
+            #Get the column we'll be operating on
+            w_i = w[:,col_i]
+            
+            #Create a new merge list for use in this function, basically takes care of
+            #interference from previous splits.
+            orig = to_merge[which_split]
+            current_merge = to_merge[:]
+            to_remove = list(np.sort(previously_split))
+            to_remove.reverse()
+            [current_merge.remove(current_merge[i]) for i in to_remove]
+            which_split = current_merge.index(orig)
+            
+            #Get the w of interest, its index, and remove it from the array.
+            w_ind = -(len(current_merge) - which_split)#Get the index of the w we want to split
+            w_val = w_i[w_ind]#Get the w that we want to split.
+            new_cols = current_merge[which_split]#Figure out which cols to add
+            w_ret = np.delete(w_i, w_ind)
+            
+            #Get a flat list of all the other merged columns
+            flat_merged = [item for i, sublist in enumerate(current_merge) for item in sublist if i != which_split]
+            
+            #Get the indices of the weight copies, adjusted for any other mergers and for earlier additions
+            new_inds = [col - sum([x < col for x in flat_merged]) - i for i, col in enumerate(new_cols)]
+            w_new = np.insert(w_ret, new_inds, w_val)
+            
+            fixed_cols.append(w_new)
+        
+        w_ret = np.array(fixed_cols).T
+        
+        return(w_ret)
+        
+    def split(self, target):
+        """
+        De-merges the target merger, copying its weight to the columns which 
+        used to compose it.
+        
+        :type target: uint
+        :param target: Index of the "to_merge" list in the "to_merge" object
+        passed to the class constructor to be split. Refers to ORIGINAL position.
+        """
+        
+        self.layers[0].set_w(self._de_merge_w(self.layers[0].w.get_value(), self.to_merge, target, self.prev_split))
+        self.current_merge.remove(self.to_merge[target])
+        self.prev_split.append(target)
+                
 
 class Layer(object):
     """
@@ -119,8 +241,9 @@ class Layer(object):
         :type len_out: int
         :param len_out: Represents dimensionality of output space of this layer
         
-        :type ID: int
-        :param ID: ID of this layer, for debugging purposes.
+        :type ID: str
+        :param ID: ID of this layer, for debugging purposes. Set to 0 for input, and '-1'
+        for output layer (not required).
         
         :type activation: function
         :param activation: Nonlinearity to be applied at this layer. Default is 
@@ -133,12 +256,29 @@ class Layer(object):
         self.output = activation(self.z)
         
         #Store dims/ID
-        self.ID = ID
+        self.ID = str(ID)
         self.len_in = len_in
         self.len_out = len_out
         
+    def set_w(self, new_val):
+        """
+        Set the weight vector of this layer to some new matrix intelligently. 
+        Stores the new dims so they are printed to the interpreter if this layer
+        is fed to it.
+        
+        Does NOT check that the new value agrees with the next layer.
+        
+        :type new_val: numpy.ndarray
+        :param new_val: new matrix (2d np.array) that the weight vector should take.
+        """
+        self.len_in = np.shape(new_val)[0]
+        self.len_out = np.shape(new_val)[1]
+        self.w.set_value(new_val)
+        
         
     def __str__(self):
-        return("Layer ID: " + str(self.ID) + " of dim " + str(self.len_in) + "x" + str(self.len_out))
+        layer_type = 'Input' if self.ID == '0' else 'Hidden'
+        layer_type = 'Output' if self.ID == '-1' else layer_type
+        return(layer_type + "layer with ID " + str(self.ID) + " of dim " + str(self.len_in) + "x" + str(self.len_out))
         
     __repr__ = __str__
